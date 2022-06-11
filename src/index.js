0790f5cd288581fa3972c7c2bbf3a31f0f640e42
Camilo Ovalle <camilo.erique.ovalleqgmail.com>
2022-06-11 16:28:32 -0500

    sigo con el mismo error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './components/App'
+import reactDOM from 'react-dom';
+import App from './components/App';
 
-const container = document.getElementById("app");
-const root = createRoot(container);
-root.render(<App />);
+reactDOM.React(<App />, document.getElementById('app'));
