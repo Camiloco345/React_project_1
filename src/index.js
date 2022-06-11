a3c313da4784c2d5981a87968fabe912f698fd4d
Camilo Ovalle <camilo.erique.ovalleqgmail.com>
2022-06-11 18:50:09 -0500

    no se que hacer con este error

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import reactDOM from 'react-dom';
 import App from './components/App';
 
-reactDOM.React(<App />, document.getElementById('app'));
+reactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -18,7 +18,7 @@ module.exports = {
         //Seccion de REGLAS
         rules:[
             {
-                test: /\.(js|jsx)$/,
+                test: /\.js$/,
                 exclude: '/node_modules/',
                 use: {
                     loader: 'babel-loader'
